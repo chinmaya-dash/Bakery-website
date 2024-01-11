5480b856967f49c44d72072fefce5bdf57221e78
Smruti Prakash Rout <smrutiprakashrout3@gmail.com>
2024-01-11 18:28:49 +0530

    add review & fix add to cart btn

--- main.js
+++ main.js
@@ -56,30 +56,14 @@ $(document).ready(function () {
 });
 //home page sliding section
 
-//Review page sliding section
 
-var slide_index = 1;
-displaySlides(slide_index);
 
-function nextSlide(n) {
-    displaySlides(slide_index += n);
+let currentSlide = 0;
+
+function changeSlide(n) {
+    const slides = document.querySelector('.slides');
+    const totalSlides = document.querySelectorAll('.slide').length;
+    currentSlide = (currentSlide + n + totalSlides) % totalSlides;
+    slides.style.transform = `translateX(-${currentSlide * 100}%)`;
 }
-
-function currentSlide(n) {
-    displaySlides(slide_index = n);
-}
-
-function displaySlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("showrSlide");
-    if (n > slides.length) { slide_index = 1 }
-    if (n < 1) { slide_index = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slide_index - 1].style.display = "block";
-}
-
-
-
-//Review page sliding section
+window.changeSlide=changeSlide;
