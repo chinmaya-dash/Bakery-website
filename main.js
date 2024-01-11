e5add2c9e206eb25568e4f72a68e71947dfa4e67
chinmaya-dash <chinmayadash442004@gmail.com>
2024-01-11 16:18:44 +0530

    000000000000000000

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ cardct.innerHTML=display.join("");
 
 
 
-
+//home page sliding section
 let slideIndex = 0;
 showSlides();
 
@@ -52,3 +52,32 @@ $(document).ready(function () {
       $(this).addClass('active');
   });
 });
+//home page sliding section
+
+//Review page sliding section
+
+var slide_index = 1;
+displaySlides(slide_index);
+
+function nextSlide(n) {
+    displaySlides(slide_index += n);
+}
+
+function currentSlide(n) {
+    displaySlides(slide_index = n);
+}
+
+function displaySlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("showrSlide");
+    if (n > slides.length) { slide_index = 1 }
+    if (n < 1) { slide_index = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slide_index - 1].style.display = "block";
+}
+
+
+
+//Review page sliding section
