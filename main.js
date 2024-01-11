6792c799ef70a94d0d52abb8f0b09e481e150493
Smruti Prakash Rout <smrutiprakashrout3@gmail.com>
2024-01-11 20:00:20 +0530

    dynamic review

--- main.js
+++ main.js
@@ -67,3 +67,82 @@ function changeSlide(n) {
     slides.style.transform = `translateX(-${currentSlide * 100}%)`;
 }
 window.changeSlide=changeSlide;
+
+
+
+
+// review mapping data
+const testimonialsData = [
+  {
+    id: 1,
+    name: "chinmaya dash",
+    rating: 4, 
+    review: "This is a great place and a must-try for biscuits and cakes. The ambiance is cozy, and the staff is friendly.",
+    img: "path/to/image1.jpg", 
+  },
+  {
+    id: 2,
+    name: "John Doe",
+    rating: 5,
+    review: "I had an amazing experience at this place. The food was delicious, and the service was impeccable.",
+    img: "path/to/image2.jpg",
+  },
+  {
+    id: 3,
+    name: "Jane Smith",
+    rating: 3,
+    review: "The atmosphere was pleasant, but the food quality could be improved. It's an average experience overall.",
+    img: "path/to/image3.jpg",
+  },
+  {
+    id: 4,
+    name: "Sam Johnson",
+    rating: 4,
+    review: "Visited with friends and enjoyed every moment. The staff was accommodating, and the menu had a variety of options.",
+    img: "path/to/image4.jpg",
+  },
+  {
+    id: 5,
+    name: "Emily White",
+    rating: 2,
+    review: "Not satisfied with the products. The taste was below expectations, and the prices were on the higher side.",
+    img: "path/to/image5.jpg",
+  },
+  {
+    id: 6,
+    name: "Alex Green",
+    rating: 5,
+    review: "Had a wonderful time at this place. The food was top-notch, and the atmosphere was perfect for a relaxing evening.",
+    img: "path/to/image6.jpg",
+  }
+];
+
+
+
+const reviewdiv = document.querySelector(".slides");
+const viewreview = testimonialsData.map((item, index) => {
+    return `
+    <div class="slide">
+    <div class="testimonial">
+      <div class="revcontainer">
+        <div class="showrSlide fade">
+          <div class="cot">
+            <div class="profilepic">
+              <img src="${item.img}" alt="invalid">
+            </div>
+            <div class="flexb_star">
+              ${Array.from({ length: item.rating }, (_, index) => (
+                `<span key=${index} class="material-icons star">star</span>`
+              )).join('')}
+            </div>
+          </div>
+          <p id="name">${item.name}</p>
+          <p>${item.review}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+`
+});
+
+reviewdiv.innerHTML = viewreview.join("");
